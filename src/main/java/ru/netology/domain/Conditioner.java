af18697f0d25983cba63ff696fb0ab061382b54a
Xenia Bocharova <bochxenia@yandex.ru>
2020-09-17 19:52:04 +0300

    added files

---
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -0,0 +1,9 @@
+package ru.netology.domain;
+
+public class Conditioner {
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+    private boolean on;
+}
