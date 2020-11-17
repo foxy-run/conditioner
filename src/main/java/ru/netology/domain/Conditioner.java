2a5d5e328e5da8726882143fd855e7ce37372524
Xenia Bocharova <bochxenia@yandex.ru>
2020-11-17 22:59:56 +0300

    Add changes

--- src/main/java/ru/netology/domain/Conditioner.java
+++ src/main/java/ru/netology/domain/Conditioner.java
@@ -37,25 +37,15 @@ public class Conditioner {
     }
 
     public void setCurrentTemperature(int currentTemperature) {
-        this.currentTemperature = currentTemperature;
-    }
-
-    public void setTemperatureMin() {
-        int currentTemperature = this.currentTemperature;
         if (currentTemperature < minTemperature) {
             return;
         }
-        this.currentTemperature = currentTemperature;
-    }
-
-    public void setTemperatureMax() {
-        int currentTemperature = this.currentTemperature;
         if (currentTemperature > maxTemperature) {
             return;
         }
         this.currentTemperature = currentTemperature;
-    }
 
+    }
     public void increaseCurrentTemperature() {
         int currentTemperature = this.currentTemperature;
         if (currentTemperature >= maxTemperature) {
